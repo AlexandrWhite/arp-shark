178d6f61bfddbc9d3cf2a7593c6b4b38b1623b6f
Alexandr Belyy <sashabelyy123@gmail.com>
2024-11-20 16:47:31 +0500

    Внес правки по интерфейсу от пожеланий

--- flaskr/arp-date.py
+++ flaskr/arp-date.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 
 def get_data(mac_address, period_name, period_value):
     query = f'SELECT event_time, INET_NTOA(switch_ip) as switch_ip, mac_addr, \
-        port_name, oper_id, vlan_id FROM event where mac_addr=\'{mac_address}\''
+        port_name, oper_id, vlan_id, port_id FROM event where mac_addr=\'{mac_address}\''
     if period_name == 'days':
         query += f'and event_time >= NOW() - INTERVAL {period_value} DAY'
     if period_name == 'months':
@@ -30,32 +30,29 @@ def get_data(mac_address, period_name, period_value):
     return result
 
 
+
+
+
+from datetime import datetime 
 def solve_open_conflicts(data):
-    pass 
-def clear_data(data):
-    data = solve_open_conflicts(data)
+    #У меня не получилось нормально работать с типом CursorResult
+    d = []
+    for row in data:
+        date = datetime.fromisoformat(str(row[0]))
+        d.append({
+            'event_time':date.strftime('%d-%m-%Y  %H:%M:%S'),
+            'switch_ip':row[1],
+            'mac_addr':row[2],
+            'port_name':row[3],
+            'oper_id':row[4],
+            'vlan_id':row[5],
+            'port_id':row[6]            
+        })
+    return d
 
 
-def get_intervals(mac_address):
-    query = f"""
-        SELECT sub2.time_op1 as start, sub2.time_op2 as finish  FROM 
-    (SELECT  
-        t1.event_time as time_op1,
-        t2.event_time as time_op2
-    FROM
-        (SELECT  event_time,mac_addr,oper_id 
-            from mac_notif.event WHERE mac_addr ='{mac_address}') t1
-    JOIN  (SELECT  event_time,mac_addr,oper_id 
-            from mac_notif.event WHERE mac_addr ='{mac_address}') t2
-    ON t2.oper_id =2 AND t2.event_time  > t1.event_time 
-    WHERE  
-        t1.oper_id = 1) sub2
-    GROUP BY 
-        start
-    """
-    query = text(query)
-    result = db.session.execute(query)
-    return result
+
+
 
 
 
@@ -73,9 +70,13 @@ def mac_table():
     period_value = request.form['period-unit']
     period_name = request.form['period-names']
 
+
+
     result = get_data(mac_address,period_name,period_value)
-    intervals = get_intervals(mac_address)
-    return render_template('test.html', events=result,intervals = intervals)
+    result = solve_open_conflicts(result)
+
+    #intervals = get_intervals(mac_address)
+    return render_template('test.html', events=result)
 
 
 @app.route('/')
